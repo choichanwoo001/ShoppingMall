5c39f09bb61c7a9790681f94642039a24b56246e
choichanwoo001 <114642968+choichanwoo001@users.noreply.github.com>
2025-07-10 23:37:12 +0900

    불필요한 코드 삭제

--- src/main/java/com/example/fastcampusbookstore/FastCampusBookStoreApplication.java
+++ src/main/java/com/example/fastcampusbookstore/FastCampusBookStoreApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FastCampusBookStoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FastCampusBookStoreApplication.class, args);
     }
